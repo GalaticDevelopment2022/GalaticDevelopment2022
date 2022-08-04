bcb54373f494693062082a6b73db750737d6fdde
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-08-03 22:10:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+import fs from "fs"
+import path from "path"
+import nodefetch from "node-fetch"
+import proxyagent from "proxy-agent"
+import request from 'request'
+const app = express();
+const agent = new proxyagent();
+const assetCache = new Map();
+const indexHTML = fs.readFileSync(path.join(__dirname, "404.html"), { encoding: "utf8" });
+var html = indexHTML;
+app.all('/d/*', function(req, res) {
+  const str = req.originalUrl;
+  const trs = str.slice('\x32');
+  console.log(trs)
+  req.pipe(request("https://discord.com" + trs)).pipe(res);
+});
+app.all('/sticker*', function(req, res) {
+  const str = req.originalUrl;
+  const trs = str;
+  req.pipe(request("https://discord.com" + trs)).pipe(res);
+});
+app.all('/asset*', function(req, res) {
+  const str = req.originalUrl;
+  const trs = str;
+  req.pipe(request("https://discord.com" + trs)).pipe(res);
+});
+app.all("*", (req, res) => {
+  res.send(html);
+});
+app.listen(2022);
